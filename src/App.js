3a974d9253df93c6b723ac3eb031bd829d5325e8
Andrii Khomenko <khomenkoam007@gmail.com>
2021-09-11 19:29:35 +0300

    double-sided binding and useState

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function App() {
 
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('текст в инпуте')
 
   function increment() {
 
@@ -18,6 +19,12 @@ function App() {
       <h1>{likes}</h1>
       <button onClick={ increment }>Increment</button>
       <button onClick={ decrement }>Decrement</button>
+
+      <h1>{value}</h1>
+      <input  type="text" 
+              value={value}
+              onChange={event => setValue(event.target.value)}
+      />
     </div>
   );
 }
