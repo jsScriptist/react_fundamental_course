d16b99e8bf7c0b1f4d7e05fb3259f46b2da5d8e7
Andrii Khomenko <khomenkoam007@gmail.com>
2021-09-14 23:40:18 +0300

    Add a class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from './components/Counter';
 
 function App() {
@@ -30,6 +31,8 @@ function App() {
       <Counter />
       <Counter />
       <Counter />
+      <hr />
+      <ClassCounter/>
     </div>
   );
 }
